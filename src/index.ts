ae14e8ccc73fdf34452dd6690729b5c8a3aadc3e
SharpDevOps10 <td090833@gmail.com>
2023-04-27 13:45:18 +0300

    Added query parameters

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import express, {Request, Response} from 'express';
 const app = express();
 const port = process.env.PORT || 5000;
 
-const products = [{title: 'apricot'}, {title: 'orange'}];
+const products = [{id: 1, title: 'apricot'}, {id: 1, title: 'orange'}];
 const addresses = [{id: 1, value: 'Abdul 13'}, {id: 2, value: 'Sel 33'}];
 app.get('/products', (req: Request, res: Response) => {
-  res.send(products);
+  if (req.query.title) {
+    const searchString = req.query.title.toString();
+    res.send(products.filter((p) => p.title.indexOf(searchString) > -1));
+  } else {
+    res.send(products);
+  }
 });
 
-app.get('/products/:productTitle', (req: Request, res: Response) => {
-  const product = products.find((p) => p.title === req.params.productTitle);
+app.get('/products/:id', (req: Request, res: Response) => {
+  const product = products.find((p) => p.title === req.params.id);
   if (product) res.send(product);
   else res.send(404);
 });
