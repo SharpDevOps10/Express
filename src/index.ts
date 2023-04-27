bdeb7527b6309fb2148fe46193c7e6a26087b9e2
SharpDevOps10 <td090833@gmail.com>
2023-04-27 12:52:59 +0300

    Updated file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, {Request, Response} from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
+const products = [{title: 'apricot'}, {title: 'orange'}];
+const addresses = [{value: 'Abdul 13'}, {value: 'Sel 33'}];
+app.get('/products', (req : Request, res : Response) => {
+  res.send(products);
 });
 
+app.get('/addresses', (req : Request, res : Response) => {
+  res.send(addresses);
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
