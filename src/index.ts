7e622b0c572d6593c8fed209b4017942fb666a3e
SharpDevOps10 <td090833@gmail.com>
2023-04-27 13:15:55 +0300

    Added `products`

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import express, {Request, Response} from 'express';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 const products = [{title: 'apricot'}, {title: 'orange'}];
 const addresses = [{value: 'Abdul 13'}, {value: 'Sel 33'}];
-app.get('/products', (req : Request, res : Response) => {
+app.get('/products', (req: Request, res: Response) => {
   res.send(products);
 });
 
-app.get('/addresses', (req : Request, res : Response) => {
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+  const product = products.find((p) => p.title === req.params.productTitle);
+  res.send(product);
+});
+
+app.get('/addresses', (req: Request, res: Response) => {
   res.send(addresses);
 });
 app.listen(port, () => {
