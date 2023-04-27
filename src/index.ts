6c32db3ae02b196099961b6f66cbbdc6403f5ccf
SharpDevOps10 <td090833@gmail.com>
2023-04-27 13:34:01 +0300

    Added addresses id

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const products = [{title: 'apricot'}, {title: 'orange'}];
-const addresses = [{value: 'Abdul 13'}, {value: 'Sel 33'}];
+const addresses = [{id: 1, value: 'Abdul 13'}, {id: 2, value: 'Sel 33'}];
 app.get('/products', (req: Request, res: Response) => {
   res.send(products);
 });
 
 app.get('/products/:productTitle', (req: Request, res: Response) => {
-  let product = products.find((p) => p.title === req.params.productTitle);
+  const product = products.find((p) => p.title === req.params.productTitle);
   if (product) res.send(product);
-  else res.status(404);
+  else res.send(404);
 });
 
 app.get('/addresses', (req: Request, res: Response) => {
   res.send(addresses);
+});
+app.get('/addresses/:id', (req: Request, res: Response) => {
+  const address = addresses.find((p) => p.id === +req.params.id);
+  if (address) res.send(address);
+  else res.send(404);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
