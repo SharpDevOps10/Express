33b980efc14f2c0be276ad7a4c966a546bd1c5aa
SharpDevOps10 <td090833@gmail.com>
2023-06-24 11:26:11 +0300

    Refactor: Improve code style: * move title validation into new variable * create middleware for input validation

--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,18 +1,19 @@
 import { Request, Response, Router } from 'express';
 import { productsRepository } from '../repositories/products-repository';
 import { body } from 'express-validator';
+import { inputValidation } from '../middlewares/input-validation-middleware';
 
 export const productsRouter = Router({});
 
+const titleValidation = body('title')
+  .isLength({ min: 3, max: 10 })
+  .withMessage('Length should be between 3 and 10');
+
 productsRouter.post(
   '/',
-  body('title')
-    .isLength({ min: 3, max: 10 })
-    .withMessage('Length should be between 3 and 10'),
+  titleValidation,
+  inputValidation,
   (req: Request, res: Response) => {
-    if (!req.body.title.trim()) {
-      res.send(400).send({ message: 'title is required' });
-    }
     const newProduct = productsRepository.createProduct(req.body.title);
     res.status(201).send(newProduct);
   }
@@ -34,18 +35,24 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
   }
 });
 
-productsRouter.put('/:id', (req: Request, res: Response) => {
-  const isUpdated = productsRepository.updateProduct(
-    +req.params.id,
-    req.body.title
-  );
-  if (isUpdated) {
-    const product = productsRepository.getProductById(+req.params.id);
-    res.send(product);
-  } else {
-    res.send(404);
+productsRouter.put(
+  '/:id',
+  titleValidation,
+  inputValidation,
+  (req: Request, res: Response) => {
+    const isUpdated = productsRepository.updateProduct(
+      +req.params.id,
+      req.body.title
+    );
+
+    if (isUpdated) {
+      const product = productsRepository.getProductById(+req.params.id);
+      res.send(product);
+    } else {
+      res.send(404);
+    }
   }
-});
+);
 
 productsRouter.delete('/:id', (req: Request, res: Response) => {
   const isDeleted = productsRepository.deleteProduct(+req.params.id);
